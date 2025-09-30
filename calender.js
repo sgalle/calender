c4be5e326e5d352cae3ed1fc4dbb79807c9cd5e8
Simon Galle <simongalle@MacBook-Air-von-Simon.local>
2025-09-30 14:15:48 +0200

    ..

--- calender.js
+++ calender.js
@@ -1,125 +1,134 @@
-isLeapYear = (year) => ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
-const cur_date = new Date();
-const MONTHS_DAYS = {1:31
-    , 2:(isLeapYear(cur_date.getFullYear()) ? 29 :28)
-    , 3:31
-    , 4:30
-    , 5:31
-    , 6:30
-    , 7:31
-    , 8:31
-    , 9:30
-    , 10:31
-    , 11:30
-    , 12:31
-};
+function initCalender(id="calender", cur_date = new Date(), cssDateStyle = "date-simple", lang="de", cb_dayFieldClick=null){
+    let calender = document.getElementById(id);
+    const TODAY = new Date();
+    
+    const isLeapYear = (year) => ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+    const MONTHS_DAYS = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31};
+    
+    const getKw = (date) => {
+        const jan1st = new Date(date.getFullYear(), 0, 1);
+        jan1st.setDate(jan1st.getDate() + (jan1st.getDay() % 7));
+        return Math.round((date - jan1st) / (7 * 24 * 3600 * 1000));
+      };
 
-const weekOfYear = (date) => {
-    const startOfYear = new Date(date.getFullYear(), 0, 1);
-    startOfYear.setDate(startOfYear.getDate() + (startOfYear.getDay() % 7));
-    return Math.round((date - startOfYear) / (7 * 24 * 3600 * 1000));
-  };
+    
+    const WORDS_DE = {'weekdays' : ['Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag','Sonntag']
+        , 'months' : ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September','Oktober','November','Dezember']
+        , 'cw' : 'KW'
+    };
 
-function initCalender(cssDateStyle = "date-simple"){
-    let calender = document.getElementById("calender");
+    const WORDS_ENG = {'weekdays' : ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+        , 'months' : ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September','October','November','December']
+        , 'cw' : 'CW'
+    };
+
+    let WORDS = WORDS_DE;
+
+    switch(lang){
+        case 'eng':
+            WORDS = WORDS_ENG;
+            break;
+        default:
+    }
+    document.documentElement.style.setProperty('--prefix-CW', "'"+WORDS['cw']+"'");
+
+    const selMonthId = "sel_"+calender.id + "_month";
+    const selYearId = "sel_"+calender.id + "_year";
 
     let control = document.createElement("DIV");
     control.classList.add("control");
-    control.innerHTML = "<div><select>\
-        <option value=1>Januar</option>\
-        <option value=2>Februar</option>\
-        <option value=3>März</option>\
-        <option value=4>April</option>\
-        <option value=5>Mai</option>\
-        <option value=6>Juni</option>\
-        <option value=7>Juli</option>\
-        <option value=8>August</option>\
-        <option value=9>September</option>\
-        <option value=10>Oktober</option>\
-        <option value=11>November</option>\
-        <option value=12>Dezember</option>\
-        </select><select>\
-        <option value=2020>2020</option>\
-        <option value=2021>2021</option>\
-        <option value=2022>2022</option>\
-        <option value=2023>2023</option>\
-        <option value=2024>2024</option>\
-        <option value=2025>2025</option>\
-        <option value=2026>2026</option>\
-        <option value=2027>2027</option>\
-        <option value=2028>2028</option>\
-        <option value=2029>2029</option>\
-        </select></div>"
-    control.innerHTML += "<div><i></i><i>Mo</i><i>Di</i><i>Mi</i><i>Do</i><i>Fr</i><i>Sa</i><i>So</i></div>"
+    control.innerHTML = "<div><select id='"+selMonthId+"'></select><select id='"+selYearId+"'></select></div>";
+    control.innerHTML += "<div><i></i></div>"
+
+    WORDS['months'].forEach(el =>control.firstChild.firstChild.innerHTML += "<option seleceted value="+WORDS['months'].indexOf(el)+" "+(WORDS['months'].indexOf(el) == cur_date.getMonth() ? 'selected' : '')+">"+el+"</option>");
+    for(let y = 2020; y < 2030; y++){control.firstChild.lastChild.innerHTML += "<option value="+y+" "+(y == cur_date.getFullYear() ? 'selected' : '')+">"+y+"</option>";}
+    WORDS['weekdays'].forEach(el =>control.lastChild.innerHTML += "<i>"+el.substring(0,2)+"</i>")
+
     calender.append(control);
 
-    control.firstChild.firstChild.onchange = (ev) =>{
-        alert(control.firstChild.firstChild.value )
-    }
     let dates = document.createElement("DIV");
+    dates.id = calender.id + "_divDates";
     dates.classList.add("dates");
     dates.classList.add("date-simple");
     calender.append(dates);
 
-    let curMonth = cur_date.getMonth() + 1;
-    let curMonthDays = MONTHS_DAYS[curMonth];
-    
-    let fields = 6*7*12; // 6=rows,7=weekdays,12=months
-    const WEEKDAYS = {1:'Mo', 2:'Di', 3:'Mi',4:'Do',5:'Fr', 6:'Sa', 7:'So'};
-
     let tmpDate = new Date();
-    tmpDate.setDate(1)
-    let firstKwOfMonth = weekOfYear(tmpDate);
-    console.log()
-    let firstMonthWeekday = tmpDate.getDay()-1;
-    firstMonthWeekday = (firstMonthWeekday == -1 ? 6 : firstMonthWeekday) + 1;
-    tmpDate.setDate(0)
-    let lastMonthDay = tmpDate.getDate();
-    
-    let dateCounter = 0;
 
-    function createKwField(row){
-        let divKw = document.createElement("DIV");
-        divKw.innerHTML = "<span>"+(firstKwOfMonth + row)+"</span>";
-        return divKw;
-    }
-    function createDayField(date, weekday, dayMonth=0, cb_onclick=null){
-        if(weekday < 1) return createKwField(0);
-        let divDay = document.createElement("DIV");
-        divDay.id = calender.id +"-datefield-"+tmpDate.getFullYear()+"-"+curMonth+"-"+date;
-        divDay.classList.add(cssDateStyle);
-        divDay.classList.add(dayMonth == 0 ? "cur-month-day" : (dayMonth == -1 ? "last-month-day" : "next-month-day"));
-        if(date  == cur_date.getDate()) divDay.classList.add("cur-day");
-        divDay.title = WEEKDAYS[weekday] ;
-        divDay.innerHTML = "<span>"+date+"</span><div style='display:none;'>"+date+"</div>"
+    function updateCalenderFields(){
 
-        divDay.onclick = () =>{if(cb_onclick) cb_onclick(divDay);}
-        return divDay;
-    }
-    let weekdayIter = 0;
-    for(weekdayIter;weekdayIter <= 7; weekdayIter++){
-        if(firstMonthWeekday == weekdayIter)
-            break;
-
-        let date = lastMonthDay - weekdayIter;
-        dates.append(createDayField(date, weekdayIter, -1));
-    }
-    let dayMonthIndicator = 0;
-    for(let row=0; row < 6;row++){
-        for(let weekday = weekdayIter; weekday <= 7; weekday++){
-            if(weekday == 0){
-                dates.append(createKwField(row));
-                continue
-            }
-            if(dateCounter < curMonthDays){
-                ++dateCounter;
-            }else{
-                dateCounter = 1;
-                dayMonthIndicator = 1
-            }
-            dates.append(createDayField(dateCounter, weekday, dayMonthIndicator));
+        const selYear = document.getElementById(selYearId).value;
+        const selMonth = Number(document.getElementById(selMonthId).value);
+        const selMonthDays =  (selMonth == 1 && isLeapYear(selYear) ? 29 : MONTHS_DAYS[selMonth + 1]);
+        const firstKwOfMonth = getKw(tmpDate);
+        
+        tmpDate.setFullYear(selYear);
+        tmpDate.setMonth(selMonth);
+   
+        function createKwField(cw){
+            let divKw = document.createElement("DIV");
+            divKw.innerHTML = "<span>"+cw+"</span>";
+            return divKw;
         }
-        weekdayIter = 0;
+        function createDayField(date, weekday, dayMonth=0, cb_onclick=cb_dayFieldClick){
+            let divDay = document.createElement("DIV");
+    
+            divDay.id = calender.id +"-datefield-"+selYear + "-" +selMonth + "-" + date;
+            divDay.classList.add(cssDateStyle);
+            divDay.classList.add(dayMonth == 0 ? "cur-month-day" : (dayMonth == -1 ? "last-month-day" : "next-month-day"));
+            if(date  == TODAY.getDate() && selMonth == TODAY.getMonth() && selYear == TODAY.getFullYear()) divDay.classList.add("cur-day");
+            divDay.title = WORDS['weekdays'][weekday-1] ;
+            divDay.innerHTML = "<span>"+date+"</span><div style='display:none;'>"+date+"</div>"
+    
+            divDay.onclick = () =>{if(cb_onclick) cb_onclick(divDay);}
+            return divDay;
+        }
+    
+        dates.replaceChildren();
+        
+        tmpDate.setDate(1);
+        console.log("tmpDate.getTime()", tmpDate.getFullYear(), tmpDate.getMonth(), tmpDate.getDate(), firstKwOfMonth)
+        
+        let firstMonthWeekday = tmpDate.getDay()-1;
+        firstMonthWeekday = (firstMonthWeekday == -1 ? 6 : firstMonthWeekday) + 1;
+        tmpDate.setDate(0)
+        const lastMonthDay = tmpDate.getDate();
+        
+        console.log(firstKwOfMonth, firstMonthWeekday, selMonthDays)
+        
+        let dateCounter = 0;
+        let weekdayIter = 0;
+
+        for(weekdayIter;weekdayIter <= 7; weekdayIter++){
+            if(firstMonthWeekday == weekdayIter)
+                break;
+    
+            let date = lastMonthDay - weekdayIter;
+            dates.prepend(createDayField(date, weekdayIter, -1));
+        }
+        let dayMonthIndicator = 0;
+        for(let row=0; row < 6;row++){
+            for(let weekday = weekdayIter; weekday <= 7; weekday++){
+                if(weekday == 0){
+                    // console.log(firstKwOfMonth, row)
+                    dates.append(createKwField(firstKwOfMonth+row));
+                    continue
+                }
+                if(dateCounter < selMonthDays){
+                    ++dateCounter;
+                }else{
+                    dateCounter = 1;
+                    dayMonthIndicator = 1
+                }
+                dates.append(createDayField(dateCounter, weekday, dayMonthIndicator));
+            }
+            weekdayIter = 0;
+        }
     }
+    updateCalenderFields();
+
+    control.firstChild.firstChild.onchange = (ev) =>{
+        updateCalenderFields();
+    }
+
 }
 initCalender();
